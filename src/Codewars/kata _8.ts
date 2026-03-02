a838569913c518706a77803d9fa318b2f723d2ff
Pavel Sokolov <pavel.sok77@gmail.com>
2026-03-02 14:32:36 +0200

    kata8

--- src/Codewars/kata _8.ts
+++ src/Codewars/kata _8.ts
@@ -18,3 +18,58 @@ export function removeChar(str: string): any {
     return subStr;
 }
 console.log(removeChar('avfrvl'))
+
+export function solution(str: string): string {
+    return str.split('').reverse().join('')
+}
+console.log(solution('Pael'))
+
+export function abbrevName(name: string): string {
+    let res: string[] = name.split(' ');
+    let result: string = res[0][0].toUpperCase() + '.' + res[1][0].toUpperCase()
+
+    return result
+}console.log(abbrevName('pavel sokolov'))
+
+export function isPalindrome(x: string): boolean{
+    let res: string = x.toLowerCase();
+    let result: string = res.split('').reverse().join('');
+    return result === res ? true : false;
+}console.log(isPalindrome("Abba"))
+
+export function noSpace(x: string): string {
+    let res: string = x.split(' ').join('');
+    return res
+}console.log(noSpace("KAb b  a"))
+
+export function fakeBin(x: string): string {
+    let result3: string = ''
+    for (let i = 0; i < x.length; i++) {
+        if(x[i] < "5") {
+            result3 += "0"
+        } else {
+            result3 += "1"
+        }
+
+    }return result3
+}console.log(fakeBin("158962"))
+
+export function fakeBinNew(x: string): string {
+    let digit: string[] = x.split('');
+    let result: string[] = digit.map(d => Number(d) < 5 ? "0" : "1");
+
+    return result.join('')
+} console.log(fakeBinNew("158962"))
+
+export function hoopCount(n: number): string {
+    if (n > 10) {
+        return `Great, now move on to tricks`
+    } else {
+        return `Keep at it until you get it`
+    }
+} console.log(hoopCount(15))
+
+export function trenMap(arr: number[]): number[] {
+    return arr.map(n => n * 2)
+}
+   console.log(trenMap([1,2,3,4]))
